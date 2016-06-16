69cde768ea7a1f27753d33b9a04c903a633c4088
zhouruikevin <zhouruikevin@gmail.com>
2016-06-16 20:20:44 +0800

    Android App 启动页(Splash)黑/白闪屏现象产生原因与解决办法

---
+++ app/src/main/java/com/example/makeapp/XApp.java
@@ -0,0 +1,13 @@
+package com.example.makeapp;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2016/6/16.
+ */
+public class XApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
