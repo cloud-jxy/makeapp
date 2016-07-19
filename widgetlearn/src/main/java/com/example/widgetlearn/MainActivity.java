c1a0a383a9c7ba4c5acd1bade328dd251e999bc3
zhouruikevin <zhourui_kevin@163.com>
2016-07-19 11:26:50 +0800

    标题居中

--- widgetlearn/src/main/java/com/example/widgetlearn/MainActivity.java
+++ widgetlearn/src/main/java/com/example/widgetlearn/MainActivity.java
@@ -8,17 +8,20 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar mToolbar;
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mTextView = (TextView) findViewById(R.id.tvCenter);
         initView();
 
     }
@@ -27,17 +30,17 @@ public class MainActivity extends AppCompatActivity {
         mToolbar.setTitle("Title");
         setSupportActionBar(mToolbar);
 //        mToolbar.inflateMenu(R.menu.menu_toolbar);
-        mToolbar.setSubtitle("Subtitle");
-        mToolbar.setLogo(R.mipmap.ic_star_white_24dp);
-        mToolbar.setBackgroundColor(getColor(R.color.colorPrimaryDark));
-        mToolbar.setTitleTextColor(Color.WHITE);
-        mToolbar.setNavigationIcon(R.mipmap.ic_launcher);
-        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "back", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        mToolbar.setSubtitle("Subtitle");
+//        mToolbar.setLogo(R.mipmap.ic_star_white_24dp);
+//        mToolbar.setBackgroundColor(getColor(R.color.colorPrimaryDark));
+//        mToolbar.setTitleTextColor(Color.WHITE);
+//        mToolbar.setNavigationIcon(R.mipmap.ic_launcher);
+//        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "back", Toast.LENGTH_SHORT).show();
+//            }
+//        });
         mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
@@ -58,8 +61,19 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        if (mToolbar != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
     }
 
+    @Override
+    protected void onTitleChanged(CharSequence title, int color) {
+        super.onTitleChanged(title, color);
+        if (mTextView != null) {
+            mTextView.setText("居中");
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
